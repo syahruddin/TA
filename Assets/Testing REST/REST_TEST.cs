32db4947c08c39593039ce5851d09e132c8f84ca
syahruddin <syahruddinr@gmail.com>
2023-06-09 14:43:20 +0700

    inisiasi

---
+++ Assets/Testing REST/REST_TEST.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.IO;
+
+public class REST_TEST : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://127.0.0.1:5000");
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        Debug.Log(reader.ReadToEnd());
+    }
+
+}
