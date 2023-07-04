9dc59323284cfc450759364bebe72e2fe6f3e5ef
syahruddin <syahruddinr@gmail.com>
2023-07-04 13:55:43 +0700

    connector dengan api

---
+++ Assets/Script/API_Connector.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.IO;
+
+public class API_Connector
+{
+    string host = "http://127.0.0.1:5000/";
+    public static string connect(string route){
+        string url = host + route;
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        return reader.ReadToEnd();
+    }
+}
+ 
