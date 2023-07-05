a71d96e511a4acf0ca58063b9f66df99a83af21c
syahruddin <syahruddinr@gmail.com>
2023-07-05 13:51:24 +0700

    tambah pemilihan tanggal

--- Assets/Script/Date_Selector.cs
+++ Assets/Script/Date_Selector.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Date_Selector : MonoBehaviour
 {
+    public Dropdown drop_hari,drop_bulan,drop_tahun;
     Date_Range range;
+    public System.DateTime selected_date;
     // Start is called before the first frame update
     void Start()
     {
-        get_date_range();
+        set_up();
     }
 
     // Update is called once per frame
@@ -22,14 +25,57 @@ public class Date_Selector : MonoBehaviour
         this.range = data;
         this.range.compileRange();
     }
+    void set_up(){
+        get_date_range();
+        set_tahun();
+        set_hari();
+        drop_tahun.onValueChanged.AddListener(delegate {on_year_changed();});
+        drop_bulan.onValueChanged.AddListener(delegate {on_month_changed();});
+        drop_hari.onValueChanged.AddListener(delegate {on_day_changed();});
+    }
+    void set_tahun(){
+        drop_tahun.ClearOptions();
+        List<string> tahun = new List<string>();
+        for(int i = range.startdate.Year; i <= range.enddate.Year; i++){
+            tahun.Add(i.ToString());
+        }
+        drop_tahun.AddOptions(tahun);
+    }
+    void set_hari(){
+        drop_hari.ClearOptions();
+        int bulan = 1 + drop_bulan.value;
+        int tahun = int.Parse(drop_tahun.options[drop_tahun.value].text);
+        int last_Day = System.DateTime.DaysInMonth(tahun,bulan);
+        List<string> hari = new List<string>();
+        for(int i = 1; i <= last_Day; i++){
+            hari.Add(i.ToString());
+        }
+        drop_hari.AddOptions(hari);
+    }
+    void on_year_changed(){
+        set_hari();
+        on_data_changed();
+    }
+    void on_month_changed(){
+        set_hari();
+        on_data_changed();
+    }
+    void on_day_changed(){
+
+        on_data_changed();
+    }
+    void on_data_changed(){
+        selected_date = new System.DateTime(int.Parse(drop_tahun.options[drop_tahun.value].text),1 + drop_bulan.value,1 + drop_hari.value);
+    }
     class Date_Range{
         public string start;
         public string end;
-        Waktu startdate, enddate;
+        public System.DateTime startdate, enddate;
         public void compileRange(){
-            startdate = new Waktu(start);
-            enddate = new Waktu(end);
+            startdate = System.DateTime.Parse(start);
+            enddate = System.DateTime.Parse(end);
         }
 
     }
+
 }
--- Assets/Script/Shared_data.cs
+++ Assets/Script/Shared_data.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Shared_data
 {
     public static Get_request_line_and_status_code get_request_by_ip(int day, int month, int year, string ip){
@@ -16,10 +17,11 @@ public class Waktu{
     public int tahun,bulan,hari;
     public string waktu;
     public Waktu(string waktu){
-
     }
     public Waktu(int tahun,int bulan,int hari){
-
+        this.tahun = tahun;
+        this.bulan = bulan;
+        this.hari = hari;
     }
     public bool isSameDay(Waktu patokan){
         return ((patokan.tahun == this.tahun) && (patokan.bulan == this.bulan) && (patokan.hari == this.hari));
